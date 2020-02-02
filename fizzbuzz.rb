6bef9ffcd089d34ca325a6045a941d15f3f7f8b1
Brandon Lee <eternalartistxii@yahoo.com>
2020-02-02 20:57:37 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -9,9 +9,9 @@ def fizzbuzz(int)
   elsif int % 3 == 0 # if the number int is divisible by 3
     "Fizz" # Go fizz
   
-  else int % 5 == 0
+  elsif int % 5 == 0
     "Buzz"
     
-  int == nil
+  else int = nil
   end
 end
